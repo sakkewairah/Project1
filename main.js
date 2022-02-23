7178050f4b82480de9c062a841f5ca81147a5361
AbdirahmanShirwa <78727561+AbdirahmanShirwa@users.noreply.github.com>
2022-02-23 17:55:01 +0200

    ok

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ addToDoButton.addEventListener('click', function(){
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
 
-    if (inputField) 
+    if (inputField === " ") 
     {
         alert ( " Please write something")
     }
